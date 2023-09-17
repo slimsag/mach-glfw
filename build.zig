321efd4065b57e31d8ab0bce720852c1d680d443
Stephen Gutekanst <stephen@hexops.com>
2023-09-17 13:44:52 -0700

    update to latest Zig
    Helps hexops/mach#902
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -10,47 +10,37 @@ pub fn build(b: *Build) !void {
         .source_file = .{ .path = "src/main.zig" },
     });
 
-    // TODO: uncomment this once hexops/mach#902 is fixed
-    // we cannot call b.dependency inside pub fn build if we want to use this package via the Zig
-    // package manager.
-    _ = target;
-    _ = optimize;
+    const lib = b.addStaticLibrary(.{
+        .name = "mach-glfw",
+        .root_source_file = .{ .path = "stub.c" },
+        .target = target,
+        .optimize = optimize,
+    });
+    link(b, lib);
+    b.installArtifact(lib);
 
-    // const lib = b.addStaticLibrary(.{
-    //     .name = "mach-glfw",
-    //     .root_source_file = .{ .path = "stub.c" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // lib.linkLibrary(@import("glfw").lib(b, lib.optimize, lib.target));
-    // lib.linkLibrary(b.dependency("vulkan_headers", .{
-    //     .target = lib.target,
-    //     .optimize = lib.optimize,
-    // }).artifact("vulkan-headers"));
-    // if (lib.target_info.target.os.tag == .macos) {
-    //     @import("xcode_frameworks").addPaths(b, lib);
-    // }
-    // b.installArtifact(lib);
+    const test_step = b.step("test", "Run library tests");
+    const main_tests = b.addTest(.{
+        .name = "glfw-tests",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
-    // const test_step = b.step("test", "Run library tests");
-    // const main_tests = b.addTest(.{
-    //     .name = "glfw-tests",
-    //     .root_source_file = .{ .path = "src/main.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
+    main_tests.linkLibrary(lib);
+    link(b, main_tests);
+    b.installArtifact(main_tests);
 
-    // main_tests.linkLibrary(lib);
-    // try link(b, main_tests);
-    // b.installArtifact(main_tests);
-
-    // test_step.dependOn(&b.addRunArtifact(main_tests).step);
+    test_step.dependOn(&b.addRunArtifact(main_tests).step);
 }
 
-pub fn link(b: *std.Build, step: *std.build.CompileStep) !void {
-    step.linkLibrary(@import("glfw").lib(b, step.optimize, step.target));
-    @import("glfw").addPaths(step);
+pub fn link(b: *std.Build, step: *std.build.CompileStep) void {
     if (step.target.toTarget().isDarwin()) @import("xcode_frameworks").addPaths(b, step);
+    @import("glfw").addPaths(step);
+    step.linkLibrary(b.dependency("glfw", .{
+        .target = step.target,
+        .optimize = step.optimize,
+    }).artifact("glfw"));
     step.linkLibrary(b.dependency("vulkan_headers", .{
         .target = step.target,
         .optimize = step.optimize,
